f6dda1f7cb46104142e1daeefdb3c94825ea4330
IonutAlexandru97 <aleq67@yahoo.ro>
2019-02-17 14:29:38 +0200

    cleared dependencies

--- api/users/apiGetUsers.ts
+++ api/users/apiGetUsers.ts
@@ -3,5 +3,5 @@ import { RequestHandler } from "express";
 import { usersDetail } from "../../model/shared/usersDetails";
 
 export const apiGetUsers: RequestHandler = (req, res, next) => {
-    res.json(DataStore.users.map((item: any) => new usersDetail(item)));
+    res.json(DataStore.users.map((item: any, image: any) => new usersDetail(item, image)));
 };
