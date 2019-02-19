d2a91b48b1658c628f126576b02a631a35907c85
IonutAlexandru97 <aleq67@yahoo.ro>
2019-02-19 13:27:35 +0200

    added google authentication

--- build/server.js
+++ build/server.js
@@ -2,11 +2,24 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var v1_1 = require("./api/v1/v1");
+var path = __importStar(require("path"));
+var config_1 = require("./config");
 var app = express_1.default();
+app.disable("x-powered-by");
 app.use("/v1", v1_1.routerV1);
-app.listen(process.env.PORT || 8080, function () {
-    console.log("Server started at http://locahost:8080");
+app.get("/google_signin", function (req, res, next) {
+    res.sendFile(path.join(config_1.__root, "google_signin.html"));
+});
+app.listen(process.env.PORT || 8081, function () {
+    console.log("Server started at http://locahost:8081");
 });
--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { routerV1 } from './api/v1/v1';
+import * as path from "path";
+import { __root } from './config';
 const app = express();
 
-
+app.disable("x-powered-by");
 app.use("/v1", routerV1);
 
-app.listen(process.env.PORT || 8080, () => {
-console.log("Server started at http://locahost:8080")
+app.get("/google_signin", (req, res, next) => {
+    res.sendFile(path.join(__root, "google_signin.html"));
+});
+
+app.listen(process.env.PORT || 8081, () => {
+console.log("Server started at http://locahost:8081")
 })
