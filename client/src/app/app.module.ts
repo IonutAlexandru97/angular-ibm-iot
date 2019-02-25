c64206e421cb478a6b860d67f08444c8ea63ba0f
IonutAlexandru97 <aleq67@yahoo.ro>
2019-02-25 17:40:43 +0200

    Revert changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,19 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule, MatSidenavModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
-import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
-import { HeaderComponent } from './components/header/header.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    HomeComponent,
-    LoadingScreenComponent,
-    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
