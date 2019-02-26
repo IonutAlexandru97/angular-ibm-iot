55448d2a53d7ab0a7123b3fa76fadfda430bee05
IonutAlexandru97 <aleq67@yahoo.ro>
2019-02-26 14:01:22 +0200

    finished welcome menu

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatListModule, MatSidenavModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { MatListModule, MatSidenavModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatSliderModule, MatGridListModule } from '@angular/material';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MenuComponent } from './components/welcome/menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
+    MenuComponent,
 
   ],
   imports: [
@@ -21,7 +25,9 @@ import { MatListModule, MatSidenavModule, MatToolbarModule, MatMenuModule, MatBu
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSliderModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [
