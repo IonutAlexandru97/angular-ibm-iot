1a220937be56c9ca508d976a5e91ca89ca086e83
IonutAlexandru97 <aleq67@yahoo.ro>
2019-03-08 00:01:27 +0200

    finished landing page

--- client/src/app/components/welcome/welcome.component.ts
+++ client/src/app/components/welcome/welcome.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss',
+  './sass/main.scss',
+  './sass/noscript.scss'
+]
 })
 export class WelcomeComponent implements OnInit {
 
