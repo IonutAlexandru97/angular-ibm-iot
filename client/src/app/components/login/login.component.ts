74bc9c06d630c216a61a3be107cf5f8dc5ed1d46
IonutAlexandru97 <aleq67@yahoo.ro>
2019-03-10 17:32:53 +0200

    Finished integration of API with UI - Login Page

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserData = {}
+  constructor(private _login: LoginService) { }
 
   ngOnInit() {
+  }
+  login(){
+    this._login.login(this.loginUserData)
+    .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
   }
 
 }
