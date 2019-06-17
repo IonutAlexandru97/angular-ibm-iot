a6c5975c303e04a88bd3f0e74be17c92a9e21b5d
IonutAlexandru97 <aleq67@yahoo.ro>
2019-06-17 15:24:51 +0300

    minor changes

--- client/src/@client/services/mail.service.ts
+++ client/src/@client/services/mail.service.ts
@@ -5,7 +5,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MailService {
-
+          //Local api --> http://localhost:3000/api
+          //Cloud API --> https://server-dot-my-project-1484493585394.appspot.com/api
 private _sendMailAPI = 'https://server-dot-my-project-1484493585394.appspot.com/api/send';
 constructor(private http: HttpClient) { }
 
--- client/src/app/pages/authentication/forgot-password/forgot-password.service.ts
+++ client/src/app/pages/authentication/forgot-password/forgot-password.service.ts
@@ -5,8 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ForgotPasswordService {
-
-private _resetAPI = 'http://localhost:3000/api/reset';
+          //Local api --> http://localhost:3000/api
+          //Cloud API --> https://server-dot-my-project-1484493585394.appspot.com/api
+private _resetAPI = 'hhttps://server-dot-my-project-1484493585394.appspot.com/api/reset';
 
 constructor(private http: HttpClient) { }
 
