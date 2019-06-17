871ada393be5dd032caf22c8817d4fff33b0773b
IonutAlexandru97 <aleq67@yahoo.ro>
2019-06-17 17:32:36 +0300

    enabled cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const passport = require('passport');
 const logger = require('morgan');
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+   });
 app.use(passport.initialize());
 app.use(logger('dev'));
 
